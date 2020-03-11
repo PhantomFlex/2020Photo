0965a2d0a75b33e43e3546722117c624b0db0323
PhantomFlex <48965061+PhantomFlex@users.noreply.github.com>
2020-03-11 23:17:00 +0300

    WIP

---
+++ server/public/precache-manifest.f270875089f9134e1040dd19efa489bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7778dfbcf6ec54bc7c2987d1348edfb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af00012c3fa3e58eb0a7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25cebbe253ea31715a63",
+    "url": "/static/js/2.71f65e45.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.71f65e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af00012c3fa3e58eb0a7",
+    "url": "/static/js/main.7e3e1972.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('./'));
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log('app listening on', port);
+});
