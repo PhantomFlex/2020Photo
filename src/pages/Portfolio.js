d19ba924f9652c767bcf6c67c00cef8e3f9d69f7
PhantomFlex <48965061+PhantomFlex@users.noreply.github.com>
2020-03-28 18:16:26 +0300

    WIP

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,28 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+
 export function Portfolio () {
   return (
-    <div>
-      <h1>Портфолио</h1>
-      <li>
-        <Link to="/all">все</Link>
-      </li>
-      <li>
-        <Link to="/promo">промо</Link>
-      </li>
-      <li>
-        <Link to="/advertising">реклама</Link>
-      </li>
-      <li>
-        <Link to="/wedding">свадьбы</Link>
-      </li>
-      <li>
-        <Link to="/events">мероприятия</Link>
-      </li>
-      <li>
-        <Link to="/aircamera">съмка с воздуха</Link>
-      </li>
-    </div>
+  <div >
+    <button>Все</button>
+  </div>
   );
 };
